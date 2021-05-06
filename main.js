3f25461f1230ac10e64ee4b944264cdaaea2131e
Dustin Threet <dustin.threet@fdc1.local>
2021-05-06 13:26:09 -0500

    InvoiceRequest class setup

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 module.exports = Object.seal({
   PunchOutSetupRequest: require('./lib/PunchOutSetupRequest.js'),
   PunchOutOrderMessage: require('./lib/PunchOutOrderMessage.js'),
-  OrderRequest: require('./lib/OrderRequest.js')
+  OrderRequest: require('./lib/OrderRequest.js'),
+  InvoiceRequest: require('./lib/InvoiceRequest.js')
 })
